fd630a4a2a7a79a0d1d2bcb1d5ff8beffc955161
Kelash Kumar Khatri <khatri.kelash@yahoo.com>
2025-12-11 22:44:11 +0500

    Fix: Mock APIs hardcode for testing

--- src/main.jsx
+++ src/main.jsx
@@ -1,26 +1,30 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+import React from "react";
+import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App.jsx";
 import store from "./app/store";
-import './index.css';
-
-// start msw in dev (browser)
-// if (import.meta.env.DEV || import.meta.env.VITE_ENABLE_MOCK === "1") {
-  const { worker } = await import("./mocks/browser");
-  worker.start({
-    onUnhandledRequest: "bypass",
-  });
-// }
+import "./index.css";
 
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </Provider>
-  </React.StrictMode>,
-)
+async function enableMocking() {
+  if (
+    import.meta.env.VITE_ENABLE_MOCK === "1" ||
+    import.meta.env.VITE_ENABLE_MOCK === 1
+  ) {
+    const { worker } = await import("./mocks/browser");
+    await worker.start({ onUnhandledRequest: "bypass" });
+  }
+}
+
+enableMocking().finally(() => {
+  ReactDOM.createRoot(document.getElementById("root")).render(
+    <React.StrictMode>
+      <Provider store={store}>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </Provider>
+    </React.StrictMode>
+  );
+});

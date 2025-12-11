193100056b28ab68a9e642c6fbae685865b74286
Kelash Kumar Khatri <khatri.kelash@yahoo.com>
2025-12-11 22:27:22 +0500

    Fix: Mock APIs hardcode for testing

--- src/main.jsx
+++ src/main.jsx
@@ -7,12 +7,12 @@ import store from "./app/store";
 import './index.css';
 
 // start msw in dev (browser)
-if (import.meta.env.DEV || import.meta.env.VITE_ENABLE_MOCK === "1") {
+// if (import.meta.env.DEV || import.meta.env.VITE_ENABLE_MOCK === "1") {
   const { worker } = await import("./mocks/browser");
   worker.start({
     onUnhandledRequest: "bypass",
   });
-}
+// }
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(

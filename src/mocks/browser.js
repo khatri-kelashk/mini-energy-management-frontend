0319731591a18020acc83a22d7d68b06822099d5
Kelash Kumar Khatri <khatri.kelash@yahoo.com>
2025-12-11 23:21:59 +0500

    fix: vercel config test

--- src/mocks/browser.js
+++ src/mocks/browser.js
@@ -2,3 +2,8 @@ import { setupWorker } from "msw";
 import { handlers } from "./handlers";
 
 export const worker = setupWorker(...handlers);
+
+// Start worker in both dev and production
+if (typeof window !== 'undefined') {
+  worker.start()
+}
--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,15 @@
-import { defineConfig } from 'vite'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from "vite";
+import tailwindcss from "@tailwindcss/vite";
 
-import react from '@vitejs/plugin-react'
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(),tailwindcss(),],
-})
+  plugins: [react(), tailwindcss()],
+  publicDir: "public", // Make sure this includes mockServiceWorker.js,
+  build: {
+    rollupOptions: {
+      external: [], // Don't externalize MSW
+    },
+  },
+});
